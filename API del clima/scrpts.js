c59f513d4f726e2c05a5185db6b2578bc9407442
Jesús Eduardo Montalvo <jesum0721@gmail.com>
2024-10-04 21:09:26 -0500

    Add files via upload
    API del clima por regiones

---
+++ API del clima/scrpts.js
@@ -0,0 +1,20 @@
+const apiKey = 'f2e85567f3c19ba849e066c4e0b9ae6e'; // Copiar la API enviada al correo y pegarla aquí
+const cityInput = document.getElementById('cityInput');
+const searchButton = document.getElementById('searchButton');
+const cityElement = document.getElementById('city');
+const temperatureElement = document.getElementById('temperature');
+const descriptionElement = document.getElementById('description');
+
+searchButton.addEventListener('click', () => {
+    const city = cityInput.value;
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`)
+        .then(response => response.json())
+        .then(data => {
+            cityElement.textContent = data.name;
+            temperatureElement.textContent = `${data.main.temp}°C`;
+            descriptionElement.textContent = data.weather[0].description;
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+});
